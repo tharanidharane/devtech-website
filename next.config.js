91a9e1dd371de11775857ae6c1ad052424d50e9c
Sravan Betanapalli <betanapallisravanraj@gmail.com>
2025-08-10 03:42:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Remove or comment out this line:
+  // output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'devtechai.org',
+          },
+        ],
+        destination: 'https://www.devtechai.org/',
+        permanent: true,
+      },
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'http://devtechai.org',
+          },
+        ],
+        destination: 'https://www.devtechai.org/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
